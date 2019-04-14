0cf775f3e24f70531088a156bd12e93c90db6cf3
piyush <bhangalepiyush@gmail.com>
2019-04-14 22:10:16 +0530

    Lemme Shutdown PC

---
+++ format.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("January has {} days" , 31);
+    println!("{0} - 1 & {1} - 2 & {0} again", "Hey" , "World");
+    println!("Rameh is {verb} {object}.",
+        verb="playing",
+        object="football"
+     )
+}
